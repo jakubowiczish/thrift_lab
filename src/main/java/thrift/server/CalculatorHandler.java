370395fada14dc2e7b293490f2e064658e8bd62f
jakubowiczish <jakub77plotnikowski@gmail.com>
2020-04-19 16:56:44 +0200

    Reformat code

--- src/main/java/thrift/server/CalculatorHandler.java
+++ src/main/java/thrift/server/CalculatorHandler.java
@@ -1,33 +1,36 @@
 package thrift.server;
 
 import org.apache.thrift.TException;
+import sr.rpc.thrift.Calculator;
 
 // Generated code
-import sr.rpc.thrift.*;
 
 public class CalculatorHandler implements Calculator.Iface {
 
-	int id;
+    int id;
 
-	public CalculatorHandler(int id) {
-		this.id = id;
-	}
+    public CalculatorHandler(int id) {
+        this.id = id;
+    }
 
-	@Override
-	public int add(int n1, int n2) {
-		System.out.println("CH#" + id + " add(" + n1 + "," + n2 + ")");
-		if(n1 > 1000 || n2 > 1000) { 
-			try { Thread.sleep(6000); } catch(java.lang.InterruptedException ex) { }
-			System.out.println("DONE");
-		}
-		return n1 + n2;
-	}
+    @Override
+    public int add(int n1, int n2) {
+        System.out.println("CH#" + id + " add(" + n1 + "," + n2 + ")");
+        if (n1 > 1000 || n2 > 1000) {
+            try {
+                Thread.sleep(6000);
+            } catch (java.lang.InterruptedException ex) {
+            }
+            System.out.println("DONE");
+        }
+        return n1 + n2;
+    }
 
-	@Override
-	public int subtract(int num1, int num2) throws TException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int subtract(int num1, int num2) throws TException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
 }
 
